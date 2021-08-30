c3f4f3efc87b8ee66e3c3f869f034ffe534504a3
Md Mehedi Hasan Hridoy <84968674+mehedihasanhr@users.noreply.github.com>
2021-08-30 21:42:42 +0600

    Update accordion.tsx

--- components/accordion.tsx
+++ components/accordion.tsx
@@ -18,17 +18,22 @@ interface Accordion {
   onClick: Function;
 }
 
-const Accordion = ({ head, items, show, onClick }: Accordion) => {
-  const [height, setHeight] = useState<number>(0);
+const Accordion = ({ head, items, show=false, onClick }: Accordion) => {
+
+  const [height, setHeight] = useState<number>(0)
   const ARef = useRef<HTMLDivElement>(null);
 
-  useEffect(() => {
-    if (ARef.current) {
-      setHeight(ARef.current.scrollHeight);
-    } else {
+  useEffect(()=>{
+    if(show === true){
+      if(ARef.current){
+        setHeight(ARef.current.scrollHeight);
+      }
+    }else{
       setHeight(0);
     }
-  }, [show]);
+  }, [show])
+
+
 
   return (
     <div className={styles.__accordion}>
@@ -45,21 +50,17 @@ const Accordion = ({ head, items, show, onClick }: Accordion) => {
           <ChevronRight strokeWidth={1} width={18} />
         )}
       </div>
-      <div
-        className={`${styles.__accordion_items}`}
-        ref={ARef}
-        style={{ height: height + "px" }}
-      >
-        <ul className={styles.__lists}>
-          {items.map((item, idx) => (
-            <li className={styles.__list} key={idx}>
-              <Link href={item.path}>
-                <a className={styles.__list_link}> - {item.title} </a>
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
+        <div className={`${styles.__accordion_items}`} ref={ARef} style={{height: height+'px'}}>
+          <ul className={styles.__lists}>
+            {items.map((item, idx) => (
+              <li className={styles.__list} key={idx}>
+                <Link href={item.path}>
+                  <a className={styles.__list_link}> - {item.title} </a>
+                </Link>
+              </li>
+            ))}
+          </ul>
+        </div>
     </div>
   );
 };
