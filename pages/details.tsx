5ded4ca8fd9adaa95c4629fa6eff9ad4381184dd
Md Mehedi Hasan Hridoy <84968674+mehedihasanhr@users.noreply.github.com>
2021-08-31 11:50:09 +0600

    msearch

---
+++ pages/details.tsx
@@ -0,0 +1,34 @@
+//css
+import styles from "../styles/Details.module.css";
+
+//icons
+
+// default components
+import Head from "next/head";
+import Link from "next/link";
+import Image from "next/image";
+import type { NextPage } from "next";
+
+//custom components
+
+//utilites
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <meta charSet="UTF-8" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title> Tarulota.com || Details </title>
+        <link rel="icon" href="/fav-icon.svg" />
+      </Head>
+
+      <div className="container">
+
+      </div>
+    </>
+  );
+};
+
+export default Home;
