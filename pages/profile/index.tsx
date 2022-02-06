2954fc4a90cdb00a2d35187a865c5f152896a7d7
mehedihasanhr <84968674+mehedihasanhr@users.noreply.github.com>
2022-02-06 15:32:05 +0600

    update

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -13,7 +13,6 @@ import { useState } from "react";
 import Button from "../../components/button";
 
 import { Users } from "../../utilites/users";
-import { useRouter } from "next/router";
 
 const Profile = () => {
   const [name, setName] = useState<string>("Sameul Islam Simu");
