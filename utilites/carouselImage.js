3baa1f499148b47aee6084f2914d8c23526de5b1
Md Mehedi Hasan Hridoy <84968674+mehedihasanhr@users.noreply.github.com>
2022-12-02 16:00:07 +0600

    update banner images

--- utilites/carouselImage.js
+++ utilites/carouselImage.js
@@ -1,6 +1,6 @@
 export const Images =[
-    {img : "/placeholder/bb.png"},
-    {img : "/placeholder/bb.png"},
-    {img : "/placeholder/bb.png"},
-    {img : "/placeholder/bb.png"},
+    {img : "/placeholder/banner-1.jpg"},
+    {img : "/placeholder/banner-2.jpg"},
+    {img : "/placeholder/banner-3.jpg"},
+    {img : "/placeholder/banner-2.jpg"},
 ]
